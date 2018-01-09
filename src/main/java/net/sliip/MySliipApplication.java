dbfe8b70155c3ef339910a18612e8b5d149a89cb
XTRM_LG\USER <saturn5903@gmailcom>
2018-01-09 14:06:17 +0900

    initial

---
+++ src/main/java/net/sliip/MySliipApplication.java
@@ -0,0 +1,12 @@
+package net.sliip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySliipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySliipApplication.class, args);
+	}
+}
---
+++ src/test/java/net/sliip/MySliipApplicationTests.java
@@ -0,0 +1,16 @@
+package net.sliip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySliipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
